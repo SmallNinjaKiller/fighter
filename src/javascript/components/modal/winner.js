5d1ac5f8117826c3f8108fcadbdfeb48f75ce478
SmallNinjaKiller <bogdan.goroshko@gmail.com>
2022-06-09 17:03:59 +0300

    changed showWinnerModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -2,13 +2,6 @@ import { showModal } from "./modal";
 
 export function showWinnerModal(fighter) {
   // call showModal function 
-  
-  showModal({
-    title: fighter.name,
-    bodyElement: `Winner is ${fighter.name}`,
-    onClose: () => {
-      const root = document.getElementById('root');
-      root.innerHTML = 'Reload page for a new game';
-    }
-  })
+
+  showModal({ title: fighter.name, bodyElement: `Winner is ${fighter.name}`, onClose: () => { location.reload() } })
 }
