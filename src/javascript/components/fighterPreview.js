977e03c33c2bd643cd72b4fdd9344f06443751f1
SmallNinjaKiller <bogdan.goroshko@gmail.com>
2022-06-09 11:23:02 +0300

    Code optimization

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -2,28 +2,16 @@ import { createElement } from '../helpers/domHelper';
 
 export function createFighterPreview(fighter, position) {
   const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-  const fighterElement = createElement({
-    tagName: 'div',
-    className: `fighter-preview___root ${positionClassName}`,
-  });
+  const fighterElement = createElement({ tagName: 'div', className: `fighter-preview___root ${positionClassName}` });
 
   // todo: show fighter info (image, name, health, etc.)
-  showFighterInfo(fighter, fighterElement)
-  return fighterElement;
+  return showFighterInfo(fighter, fighterElement);
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
-    title: name,
-    alt: name 
-  };
-  const imgElement = createElement({
-    tagName: 'img',
-    className: 'fighter-preview___img',
-    attributes,
-  });
+  const attributes = { src: source, title: name, alt: name };
+  const imgElement = createElement({ tagName: 'img', className: 'fighter-preview___img', attributes });
 
   return imgElement;
 }
